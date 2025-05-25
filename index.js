af65eba87ed914c6ebdc98c64ff99ed09d0094a8
Mohaiminul375 <mohaiminul375@gmail.com>
2025-05-25 18:14:56 +0600

    create: api error

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 const app = express();
+const port = process.env.PORT || 5000 //review
 
 
-
-connectDB();
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running at port:${port}`);
+        })
+    })
+    .catch((error) => console.error('MongoDB connection Failed!!', error))
---
+++ models/user.model.js
@@ -0,0 +1,5 @@
+import mongoose from "mongoose";
+
+const userModel = new mongoose.Schema({
+
+})
